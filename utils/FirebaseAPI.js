5fe31c02504664942401eae96f6cc38eee8b3c87
HaysS <stanfordhays@gmail.com>
2018-12-27 14:29:59 -0600

    added GeoFireAPI. Updated FirebaseAPI

--- utils/FirebaseAPI.js
+++ utils/FirebaseAPI.js
@@ -1,19 +1,61 @@
+/*
+<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
+----------------------------------------------------------------
+    Author: Hays Stanford
+    Website: www.haysstanford.com
+    Github: github.com/HaysS
+    Twitter: twitter.com/thehaysstanford
+----------------------------------------------------------------
+<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
+*/
+
 import firebase from 'firebase'
 
 //Automatically set to 'production' when published through Expo
 var env = process.env.NODE_ENV || 'development';
 // var env = 'production'
 var config = require('../config')[env];
+ 
+export function storeNewUser(user) {
+    ifUserNotFound(user.uid, () => storeUser(user))
+}
 
-export function createUser(email, password, cb) {
-    firebase.auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then( cb() )
-      .catch(error => this.setState({ errorMessage: error.message }))
-  }
+export function storeUser(user) {
+    const data = {
+        uid: user.uid,
+        phoneNumber: user.phoneNumber,
+        name: '',
+    }
 
-export function signOut() {
-	firebase.auth().signOut()
+    firebase.database().ref().child('users').child(user.uid)
+        .set({ data })
+
+    console.log('stored user.')
+}
+
+export function ifUserNotFound(uid, cb) {
+    getUser(uid, (user) => {
+        if(!user) {
+            cb()
+        } else {
+            console.log('user exist in database.')
+        }
+    })
+}
+
+export function getUser(uid, cb) { 
+    console.log('getUser called')
+
+    return firebase.database().ref().child('users').child(uid)
+        .once('value')
+        .then((snap) => {
+            cb(snap.val())
+        })
+}
+
+export function signOut(cb) {
+  firebase.auth().signOut()
+    cb()
 }
 
 export const signInWithPhoneAndCaptcha = async (phone, token, cb) => {
@@ -32,33 +74,10 @@ export const signInWithPhoneAndCaptcha = async (phone, token, cb) => {
     }
 }
 
+export const getAuth = (cb) => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+        cb(user)
+        unsubscribe()
+    })
+}
 
-// export function setInvisibleRecaptcha() {
-// 	console.log('setInvisibleRecaptcha() called.')
-// 	window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
-// 	  'size': 'invisible',
-// 	  'callback': function(response) {
-// 	    // reCAPTCHA solved, allow signInWithPhoneNumber.
-// 	    console.log('invisible reCAPTCHA success.')
-// 	    onSignInSubmit();
-// 	  }
-// 	});
-// }
-
-// export function signInWithPhoneNumber() {
-// 	var phoneNumber = '5555555555';
-// 	var appVerifier = window.recaptchaVerifier;
-
-// 	firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
-// 	    .then(function (confirmationResult) {
-// 	      // SMS sent. Prompt user to type the code from the message, then sign the
-// 	      // user in with confirmationResult.confirm(code).
-// 	      console.log('firebase signInWithPhoneNumber: Success.')
-// 	      window.confirmationResult = confirmationResult;
-// 	    }).catch(function (error) {
-// 	      // Error; SMS not sent
-// 	      // ...
-// 	      console.log('firebase signInWithPhoneNumber: Error, here is msg: ', error)
-// 	    });
-// }
-// 	
---
+++ utils/GeoFireAPI.js
@@ -0,0 +1,161 @@
+/*
+<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
+----------------------------------------------------------------
+    Author: Hays Stanford
+    Website: www.haysstanford.com
+    Github: github.com/HaysS
+    Twitter: twitter.com/thehaysstanford
+----------------------------------------------------------------
+<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>
+*/
+
+import firebase from 'firebase'
+import GeoFire from 'geofire'
+import { Location } from 'expo'
+
+import * as FirebaseAPI from './FirebaseAPI'
+
+//Automatically set to 'production' when published through Expo
+var env = process.env.NODE_ENV || 'development';
+// var env = 'production'
+var config = require('../config')[env];
+
+const OPTIONS = {
+	enableHighAccuracy: true,
+	timeInterval: 100000,
+	distanceInterval: 3000,
+}
+
+/*
+#######################################################
+--------------     DATABASE FUNCTIONS    --------------
+#######################################################
+*/
+
+export const watchLocation =  (uid) => {
+	console.log('watchLocation()')
+	return Location.watchPositionAsync(OPTIONS, (pos) => {
+		setUserLocation(uid, pos.coords.latitude, pos.coords.longitude)
+	})
+}
+
+export const setUserLocation = (uid, lat, lon) => {
+	console.log('setUserCoord()')
+	const firebaseRef = firebase.database().ref()
+	const geoFire = new GeoFire(firebaseRef.child('users/'+uid))
+
+	geoFire.set('location', [lat, lon]) //refactor this to function
+		.then(() => {
+			console.log("Key has been added to GeoFire");
+		}, (error) => {
+			console.log("Error: " + error);
+		}
+	)
+}
+
+export const setDriverLocation = (uid, lat, lon) => {
+	console.log('setDriverLocation()')
+	const firebaseRef = firebase.database().ref()
+	const geoFire = new GeoFire(firebaseRef.child('drivers/'))
+
+	geoFire.set('location', [lat, lon]) //refactor this to function
+		.then(() => {
+			console.log("Key has been added to GeoFire");
+		}, (error) => {
+			console.log("Error: " + error);
+		}
+	)
+}
+
+export const getMarkerLocation = (uid, cb) => {
+	const firebaseRef = firebase.database().ref()
+	const geoFire = new GeoFire(firebaseRef.child('drivers/'))
+
+	geoFire.get(uid).then((location) => {
+		if(location != null)
+			cb(location)
+		else
+			console.log('location not found.')
+	})
+}
+
+export const getUserLocation = (uid, cb) => {
+	console.log('get user location called')
+
+	FirebaseAPI.getUser(uid, (user) => {
+		if(user.location)
+			cb(user.location.l)
+		else
+			console.log('cannot find user coord')
+	})
+}
+
+
+/*
+#######################################################
+--------------      QUERY FUNCTIONS      --------------
+#######################################################
+// */ 
+
+const newDriver = (key, location) => {
+	// console.log('new driver: ', key, location[0], location[1])
+	const driver = {
+		uid: key,
+		location: {
+			latitude: location[0],
+			longitude: location[1],
+		},
+	}
+
+	return driver
+}
+
+export const setReadyRegistration = (geoQuery) => {
+	geoQuery.on("ready", function() {
+		console.log("GeoQuery has loaded and fired all other events for initial data");
+	});
+}
+
+export const setKeyEnteredRegistration = (geoQuery, cb) => {
+	geoQuery.on("key_entered", function(key, location, distance) {
+		console.log(key + " entered query at " + location + " (" + distance + " km from center)");
+		const driver = newDriver(key, location)
+		cb(driver)
+	});
+}
+
+export const setKeyMovedRegistration = (geoQuery, cb) => {
+	geoQuery.on("key_moved", function(key, location, distance) {
+		console.log(key + " moved within query to " + location + " (" + distance + " km from center)");
+		const driver = newDriver(key, location)
+		cb(driver)
+	});
+}
+
+export const setKeyExitedRegistration = (geoQuery, cb) => {
+	geoQuery.on("key_exited", function(key, location, distance) {
+		console.log(key + " exited query to " + location + " (" + distance + " km from center)");
+		const driver = newDriver(key, location)
+		cb(driver)
+	});
+}
+
+export const cancelGeoQuery = (geoQuery) => {
+	geoQuery.cancel()
+}
+
+export const getGeoQuery = (uid, cb) => {
+	const firebaseRef = firebase.database().ref()
+	const geoFire = new GeoFire(firebaseRef.child('drivers'))
+
+	console.log('getGeoQuery called')
+
+	getUserLocation(uid, (location) => {
+		const geoQuery = geoFire.query({
+			center: location,
+			radius: 10.5
+		})
+
+		cb(geoQuery)
+	})
+}
