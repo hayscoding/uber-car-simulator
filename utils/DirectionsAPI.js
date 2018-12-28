23707ba2d5fb752408664166742f0a12feaa1001
HaysS <stanfordhays@gmail.com>
2018-12-28 14:36:48 -0600

    new .env configuration system works as intended

--- utils/DirectionsAPI.js
+++ utils/DirectionsAPI.js
@@ -1,7 +1,5 @@
-//Automatically set to 'production' when published through Expo
-var env = process.env.NODE_ENV || 'development';
 // var env = 'production'
-var config = require('../config')[env];
+var config = require('../config');
 
 const apiKey = config.googleDirectionsAPI.key
 
